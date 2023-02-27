48b56b5b51c62eeba014c53524d214f2dd02a7f0
raven <zlat.dominic.francisc@gmail.com>
2023-02-27 02:52:27 +0200

    Added docs folder for readme required pics

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import NewAccount from './pages/NewAccount'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    document.title = 'Ring Relay'
+  })
+
   return (
     <Routes>
         <Route exact path="newAccount" element={<NewAccount/>}></Route>
