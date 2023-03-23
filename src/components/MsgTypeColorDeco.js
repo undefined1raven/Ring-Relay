7acf9674ae31e76adccd8aef45ce45842782a737
raven <zlat.dominic.francisc@gmail.com>
2023-03-23 06:34:17 +0200

    Message Type Selector UI Bug Fix

--- src/components/MsgTypeColorDeco.js
+++ src/components/MsgTypeColorDeco.js
@@ -8,16 +8,16 @@ function MsgTypeColorDeco(props) {
             <path d="M6.5 0L12.9952 3.75V11.25L6.5 15L0.00480938 11.25V3.75L6.5 0Z" fill="url(#paint2_radial_137_366)" />
             <defs>
                 <radialGradient id="paint0_radial_137_366" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(2.25 9.75) rotate(164.32) scale(14.8008)">
-                    <stop stop-color="#FF0000" />
-                    <stop offset="0.533556" stop-color="#A30000" stop-opacity="0.478096" />
+                    <stop stopColor="#FF0000" />
+                    <stop offset="0.533556" stopColor="#A30000" stopOpacity="0.478096" />
                 </radialGradient>
                 <radialGradient id="paint1_radial_137_366" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(6.5) rotate(90) scale(14.5)">
-                    <stop stop-color="#00FF00" />
-                    <stop offset="1" stop-opacity="0" />
+                    <stop stopColor="#00FF00" />
+                    <stop offset="1" stopOpacity="0" />
                 </radialGradient>
                 <radialGradient id="paint2_radial_137_366" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(13 11.25) rotate(-139.236) scale(9.57209)">
-                    <stop stop-color="#0000FF" />
-                    <stop offset="1" stop-opacity="0" />
+                    <stop stopColor="#0000FF" />
+                    <stop offset="1" stopOpacity="0" />
                 </radialGradient>
             </defs>
         </svg>
