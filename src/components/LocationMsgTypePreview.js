8d9fc52575ca82dcab7302b7604de7f11963d93a
raven <zlat.dominic.francisc@gmail.com>
2023-03-26 06:33:44 +0300

    Use device location feature added

--- src/components/LocationMsgTypePreview.js
+++ src/components/LocationMsgTypePreview.js
@@ -9,8 +9,8 @@ function LocationMsgTypePreview(props) {
             <div className="locationMsgTypePreviewContainer" style={{ backgroundColor: props.bkg }}>
                 <Label style={{ top: '55%', left: '23%' }} fontSize="1.9vh" text="Long" color={props.ghost ? "#0013BA" : "#6300E0"}></Label>
                 <Label style={{ top: '55%', left: '50%' }} fontSize="1.9vh" text="Lat" color={props.ghost ? "#0013BA" : "#6300E0"}></Label>
-                <Label style={{ top: '15%', left: '23%' }} fontSize="2.1vh" text={props.selectedLocation.ini ? parseFloat(props.selectedLocation.locationObj.long).toFixed(3) : '[UNK]'} color="#FFF"></Label>
-                <Label style={{ top: '15%', left: '50%' }} fontSize="2.1vh" text={props.selectedLocation.ini ? parseFloat(props.selectedLocation.locationObj.lat).toFixed(3) : '[UNK]'} color="#FFF"></Label>
+                <Label style={{ top: '15%', left: '23%' }} fontSize="2.1vh" text={props.selectedLocation.ini ? parseFloat(props.selectedLocation.locationObj.long).toFixed(3).toString().substring(0, 7) : '[UNK]'} color="#FFF"></Label>
+                <Label style={{ top: '15%', left: '50%' }} fontSize="2.1vh" text={props.selectedLocation.ini ? parseFloat(props.selectedLocation.locationObj.lat).toFixed(3).toString().substring(0, 7) : '[UNK]'} color="#FFF"></Label>
                 <MsgTypeLocationDeco color={props.ghost ? "#0500FF" : "#8627FF"} style={{ width: '15vh', height: '10vh', left: '-10.8%', marginTop: '2.5%' }}></MsgTypeLocationDeco>
                 <Button onClick={props.onCancel} fontSize="3.4vh" color="#929292" bkg="#929292" width="4.8vh" height="4.8vh" style={{ left: '83%', borderRadius: '5px', backgroundColor: '#00000000' }} label="×"></Button>
             </div>
--- src/components/LocationPickerOverlay.js
+++ src/components/LocationPickerOverlay.js
@@ -15,18 +15,12 @@ function getRandomInt(min, max) {
 function LocationPickerOverlay(props) {
     const [mapVisible, setMapVisible] = useState(true)
     const [loadingLabelVisible, setLoadingLabelVisible] = useState(true)
-    const [location, setLocation] = useState({ lat: 0, long: 0 })
+    const [location, setLocation] = useState({ lat: 46.774, long: 8.362, zoom: 3 })
 
-    // useEffect(() => {
-    //     setMapVisible(false);
-    //     let int = setInterval(() => {
-    //         setLocation({ lat: getRandomInt(0, 60), long: getRandomInt(0, 60) });
-    //     }, 1000);
-    // }, [])
 
     useEffect(() => {
-        console.log(location)
-    }, [location])
+        props.updateLocationInput({ viewState: { longitude: location.long, latitude: location.lat, zoom: 3 } })
+    }, [])
 
     useEffect(() => {
         if (mapVisible == false) {
@@ -35,6 +29,21 @@ function LocationPickerOverlay(props) {
             }, 80);
         }
     }, [mapVisible])
+
+    const getDeviceLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                if (position.coords.latitude && position.coords.longitude) {
+                    setLocation({ lat: position.coords.latitude, long: position.coords.longitude, zoom: 15 });
+                    setMapVisible(false);
+                    props.updateLocationInput({ viewState: { longitude: position.coords.longitude, latitude: position.coords.latitude, zoom: 15 } })
+                    setTimeout(() => {
+                        setMapVisible(true)
+                    }, 20);
+                }
+            }, () => { })
+        }
+    }
 
 
     if (props.show) {
@@ -51,9 +60,9 @@ function LocationPickerOverlay(props) {
                     onBoxZoomEnd={(e) => update(e)}
                     onDrag={(e) => update(e)} mapLib={maplibregl}
                     initialViewState={{
-                        longitude: 8.362,//8.362
-                        latitude: 46.774,//46.774
-                        zoom: 3
+                        longitude: location.long,
+                        latitude: location.lat,
+                        zoom: location.zoom
                     }}
                     style={{ position: 'absolute', width: "100%", height: "100%", borderLeft: 'solid 1px #7000FF' }}
                     mapStyle="https://api.maptiler.com/maps/fcae873d-7ff0-480b-8d6d-41963084ad90/style.json?key=R1cyh6lj1mTfNEycg2N1"
@@ -62,7 +71,7 @@ function LocationPickerOverlay(props) {
                 </Map>
                     : ''}
                 {!loadingLabelVisible ? <LocationIndi></LocationIndi> : ''}
-                {!loadingLabelVisible ? <Button bkg={props.ghost ? "#0500FF" : "#7100FF"} fontSize="2vh" width="60%" height="6%" className="locationPickerUseDeviceLocationButton" color={props.ghost ? "#0500FF" : "#7100FF"} label="Use Device Location" style={{ backgroundColor: `${props.ghost ? "rgba(15, 0, 120, 0.7)" : 'rgba(25, 0, 15, 0.7)'}`, border: `solid 1px ${props.ghost ? "#0500FF" : '#7100FF'}`, color: `${props.ghost ? '#FFF' : '#7100FF'}` }}></Button> : ''}
+                {!loadingLabelVisible ? <Button onClick={getDeviceLocation} bkg={props.ghost ? "#0500FF" : "#7100FF"} fontSize="2vh" width="60%" height="6%" className="locationPickerUseDeviceLocationButton" color={props.ghost ? "#0500FF" : "#7100FF"} label="Use Device Location" style={{ backgroundColor: `${props.ghost ? "rgba(15, 0, 120, 0.7)" : 'rgba(25, 0, 15, 0.7)'}`, border: `solid 1px ${props.ghost ? "#0500FF" : '#7100FF'}`, color: `${props.ghost ? '#FFF' : '#7100FF'}` }}></Button> : ''}
                 <Label show={loadingLabelVisible} fontSize="2vh" color="#001AFF" style={{ borderRadius: '5px', width: '50%', height: '5%' }} bkg="#001AFF30" className="mapLoadingLabel" text="[Loading Map]"></Label>
                 <Label show={!loadingLabelVisible} fontSize="1.8vh" color="#AAA" style={{ borderRadius: '5px', width: '90%', height: '5%', top: '93%', backdropFilter: 'blur(5px)' }} bkg={props.ghost ? "#0013BAAA" : "#2E0067AA"} className="mapLoadingLabel" text="The message you send will contain this view"></Label>
             </div>
