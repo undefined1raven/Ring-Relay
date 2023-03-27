dea601952efdb0e0faa8d339dfa0f5b4e2912bba
raven <zlat.dominic.francisc@gmail.com>
2023-03-27 23:35:55 +0300

    Log Details UX Testing

--- src/components/LogDetails.js
+++ src/components/LogDetails.js
@@ -1,16 +1,45 @@
 import Label from './Label';
 import Button from '../components/Button.js'
 import BackDeco from '../components/BackDeco.js'
+import { useEffect, useState } from 'react';
 
 
 function LogDetails(props) {
 
     const dateFormatter = () => {
         let date = new Date(props.logObj.tx);
-        let s = date.toString().split(' ');
-        return { date: s[0] + ' ' + s[1] + ' ' + s[2] + ' ' + s[3], time: `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}` }
+        let splittedDate = date.toString().split(' ');
+        return { date: splittedDate[0] + ' ' + splittedDate[1] + ' ' + splittedDate[2] + ' ' + splittedDate[3], time: `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}` }
     }
 
+    const sessionDetailsController = () => {
+        if (props.logObj?.details?.session != undefined) {
+            if (props.logObj.details?.session) {
+                return { status: true, label: 'Authorized', color: '#00FFD1' }
+            } else {
+                return { status: false, label: 'Revoked', color: '#FF002E' }
+            }
+        } else {
+            return { status: false, label: 'Unknown', color: '#9B9B9B' }
+        }
+    }
+
+    const exportDetailsController = () => {
+        if (props.logObj?.details?.PKShareType != undefined) {
+            var actionTypeLabel = props.logObj?.details?.PKShareType.type == 'import' ? 'Import' : 'Export'
+            if (props.logObj.details?.PKShareType.method == 'scan') {
+                return { label: 'QR Code Scan', type: actionTypeLabel }
+            } else {
+                if(actionTypeLabel == 'Import'){
+                    return { label: 'File Upload', type: actionTypeLabel }
+                }else{
+                    return { label: 'File Download', type: actionTypeLabel }
+                }
+            }
+        } else {
+            return { label: 'Unknown', type: actionTypeLabel }
+        }
+    }
 
     if (props.show) {
         return (
@@ -19,16 +48,25 @@ function LogDetails(props) {
                 <Button color="#7000FF" bkg="#7000FF" child={<BackDeco style={{ width: "29%" }} color="#7000FF" />} className="logDetailsBackBtn" onClick={props.onBack}></Button>
                 <Label fontSize="1.9vh" color="#BABABA" bkg="#6300E020" className="logDetailTypeLabel" style={{ top: '43.59375%', height: '6.09375%' }} text="Time and date"></Label>
                 <Label fontSize="1.9vh" color="#FFF" bkg="#6300E020" className="logDetailActual" style={{ top: '43.59375%', height: '3.79375%', paddingBottom: '5%' }} text={dateFormatter().date}></Label>
-                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" style={{ top: '45.99375%', height: '3.79375%'}} text={dateFormatter().time}></Label>
-                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" bkg="#7000FF20" style={{ top: 'calc(43.59375% + (1 * 7%))', height: '6.09375%'}} text={props.logObj.details.device}></Label>
-                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" bkg="#7000FF20" style={{ top: 'calc(43.59375% + (2 * 7%))', height: '6.09375%'}} text={props.logObj.details.browser}></Label>
-                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" bkg="#7000FF20" style={{ top: 'calc(43.59375% + (3 * 7%))', height: '6.09375%'}} text={props.logObj.details.os}></Label>
-                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" bkg="#7000FF20" style={{ top: 'calc(43.59375% + (4 * 7%))', height: '6.09375%'}} text={props.logObj.location.name}></Label>
+                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" style={{ top: '45.99375%', height: '3.79375%' }} text={dateFormatter().time}></Label>
+                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" bkg="#7000FF20" style={{ top: 'calc(43.59375% + (1 * 7%))', height: '6.09375%' }} text={props.logObj.details.device}></Label>
+                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" bkg="#7000FF20" style={{ top: 'calc(43.59375% + (2 * 7%))', height: '6.09375%' }} text={props.logObj.details.browser}></Label>
+                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" bkg="#7000FF20" style={{ top: 'calc(43.59375% + (3 * 7%))', height: '6.09375%' }} text={props.logObj.details.os}></Label>
+                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" bkg="#7000FF20" style={{ top: 'calc(43.59375% + (4 * 7%))', height: '6.09375%' }} text={props.logObj.ip}></Label>
+                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" bkg="#7000FF20" style={{ top: 'calc(43.59375% + (5 * 7%))', height: '6.09375%' }} text={props.logObj.location.name}></Label>
+                <Label fontSize="1.9vh" color={sessionDetailsController().color} className="logDetailActual" bkg={`${sessionDetailsController().color}20`} show={props.logObj.subtype == 'Log In'} style={{ top: 'calc(43.59375% + (6 * 7%))', height: '6.09375%' }} text={sessionDetailsController().label}></Label>
+                <Label fontSize="1.9vh" color="#FFF" className="logDetailActual" bkg="#7000FF20" show={props.logObj.subtype == 'Keys Import' || props.logObj.subtype == 'Keys Export'} style={{ top: 'calc(43.59375% + (6 * 7%))', height: '6.09375%' }} text={exportDetailsController().label}></Label>
+
+                <Button show={props.logObj.subtype == 'Log In' && sessionDetailsController().status} width="90%" height="6.125%" style={{ left: '5%', top: '92.65625%', borderRadius: '5px' }} label="Revoke" color="#FF002E" bkg="#FF002E"></Button>
+
                 <Label fontSize="1.9vh" color="#BABABA" bkg="#6300E020" className="logDetailTypeLabel" style={{ top: 'calc(43.59375% + (1 * 7%))', height: '6.09375%' }} text="Device"></Label>
                 <Label fontSize="1.9vh" color="#BABABA" bkg="#6300E020" className="logDetailTypeLabel" style={{ top: 'calc(43.59375% + (2 * 7%))', height: '6.09375%' }} text="Browser"></Label>
                 <Label fontSize="1.9vh" color="#BABABA" bkg="#6300E020" className="logDetailTypeLabel" style={{ top: 'calc(43.59375% + (3 * 7%))', height: '6.09375%' }} text="Operating System"></Label>
-                <Label fontSize="1.9vh" color="#BABABA" bkg="#6300E020" className="logDetailTypeLabel" style={{ top: 'calc(43.59375% + (4 * 7%))', height: '6.09375%' }} text="IP Location"></Label>
-            
+                <Label fontSize="1.9vh" color="#BABABA" bkg="#6300E020" className="logDetailTypeLabel" style={{ top: 'calc(43.59375% + (4 * 7%))', height: '6.09375%' }} text="IP Address"></Label>
+                <Label fontSize="1.9vh" color="#BABABA" bkg="#6300E020" className="logDetailTypeLabel" style={{ top: 'calc(43.59375% + (5 * 7%))', height: '6.09375%' }} text="IP Location"></Label>
+                <Label fontSize="1.9vh" color="#BABABA" bkg="#6300E020" className="logDetailTypeLabel" show={props.logObj.subtype == 'Log In'} style={{ top: 'calc(43.59375% + (6 * 7%))', height: '6.09375%' }} text="Session Status"></Label>
+                <Label fontSize="1.9vh" color="#BABABA" bkg="#6300E020" className="logDetailTypeLabel" show={props.logObj.subtype == 'Keys Import' || props.logObj.subtype == 'Keys Export'} style={{ top: 'calc(43.59375% + (6 * 7%))', height: '6.09375%' }} text={`${exportDetailsController().type} Type`}></Label>
+
             </div>)
     } else {
         return '';
--- src/components/SettingsLogs.js
+++ src/components/SettingsLogs.js
@@ -17,17 +17,18 @@ function SettingsLogs(props) {
     const [logsArray, setLogsArray] = useState({
         ini: true, array: [
             { severity: 'warning', type: 'Account', subtype: 'Log In', tx: Date.now(), ip: '5.00.188.058', location: 'Aiur City', details: { device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
-            { severity: 'critical', type: 'Security', subtype: 'Private Key Export', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { exportType: 'scan', device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
-            { severity: 'critical', type: 'Security', subtype: 'Private Key Export', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { exportType: 'scan', device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
+            { severity: 'critical', type: 'Security', subtype: 'Keys Export', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { PKShareType: { method: 'scan', type: 'export' }, device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
+            { severity: 'critical', type: 'Security', subtype: 'Keys Export', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { PKShareType: { method: 'scan', type: 'export' }, device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
             { severity: 'important', type: 'Security', subtype: 'Password Changed', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
             { severity: 'info', type: 'Account', subtype: 'Username Changed', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
             { severity: 'important', type: 'Security', subtype: 'Password Reset', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
-            { severity: 'critical', type: 'Security', subtype: 'Private Key Import', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { importType: 'scan', device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
+            { severity: 'critical', type: 'Security', subtype: 'Keys Import', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { PKShareType: { method: 'scan', type: 'import' }, device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
+            { severity: 'warning', type: 'Account', subtype: 'Log In', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { session: true, device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
             { severity: 'warning', type: 'Account', subtype: 'Log In', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
             { severity: 'warning', type: 'Account', subtype: 'Log In', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
             { severity: 'warning', type: 'Account', subtype: 'Log In', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
-            { severity: 'warning', type: 'Account', subtype: 'Log In', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
-            { severity: 'warning', type: 'Account', subtype: 'Log In', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
+            { severity: 'warning', type: 'Account', subtype: 'Log In', tx: Date.now(), ip: '190.15.32.51', location: { name: 'Aiur City', coords: { lat: 0, long: 0 } }, details: { session: false, device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
+            { severity: 'critical', type: 'Security', subtype: 'Keys Import', tx: Date.now(), ip: '251.22.94.23', location: { name: 'Laconia Gate', coords: { lat: 0, long: 0 } }, details: { PKShareType: { method: 'file', type: 'import' }, device: 'iPhone 12', browser: 'Chrome v102', os: 'iOS 16' } },
         ]
     });
     const [logsArrayList, setLogsArrayList] = useState([]);
@@ -38,7 +39,6 @@ function SettingsLogs(props) {
 
     if (props.show) {
         return (<div>
-            <HorizontalLine className="logsLn" color="#7000FF" width="90%" top="89.375%"></HorizontalLine>
             <HorizontalLine className="logsLn" color="#7000FF" width="90%" top="31.09375%"></HorizontalLine>
             <Label style={{ top: '18.28125%' }} className="logsFiltersLabel" fontSize="2vh" text="Type" color="#FFF" bkg="#7000FF20"></Label>
             <Label style={{ top: '25%' }} className="logsFiltersLabel" fontSize="2vh" text="Time" color="#FFF" bkg="#7000FF20"></Label>
@@ -46,9 +46,11 @@ function SettingsLogs(props) {
             <Button label="Last 24h" fontSize="2vh" color="#7000FF" style={{ left: '29.444444444%', top: '25%', color: '#FFF' }} bkg="#7000FF" width="20.833333333%" className="logsFilterButton"></Button>
             <Button label="Filter" fontSize="2vh" color="#7000FF" style={{ left: '54.722222222%', top: '18.28125%', color: '#9B9B9B' }} bkg="#340076" width="20.833333333%" className="logsFilterButton"></Button>
             <Button label="Filter" fontSize="2vh" color="#7000FF" style={{ left: '54.722222222%', top: '25%', color: '#9B9B9B' }} bkg="#340076" width="20.833333333%" className="logsFilterButton"></Button>
-
-            <Button onClick={props.onBack} label="Back" className="logsButton" style={{ top: '91.71875%', height: '6.25%', width: '43.055555556%', left: '5%' }} color="#929292"></Button>
-            <Button label="Settings" className="logsButton" style={{ top: '91.71875%', height: '6.25%', width: '43.055555556%', left: '51.944444444%' }} color="#7100FF" bkg="#7100FF"></Button>
+            {!showLogDetails.show ? <>
+                <HorizontalLine className="logsLn" color="#7000FF" width="90%" top="89.375%"></HorizontalLine>
+                <Button onClick={props.onBack} label="Back" className="logsButton" style={{ top: '91.71875%', height: '6.25%', width: '43.055555556%', left: '5%' }} color="#929292"></Button>
+                <Button label="Settings" className="logsButton" style={{ top: '91.71875%', height: '6.25%', width: '43.055555556%', left: '51.944444444%' }} color="#7100FF" bkg="#7100FF"></Button>
+            </> : ''}
 
             {!showLogDetails.show ? <>
                 <Label fontSize="1.9vh" text="Info" className="logsFiltersLabel" style={{ top: '32.34375%', height: '2.34375%', paddingRight: '0.2%', paddingLeft: '0.2%' }} color="#9644FF" bkg="#9644FF30"></Label>
