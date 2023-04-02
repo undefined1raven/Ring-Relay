d97dd851bb780f04e766239127ca07989566ad4e
raven <zlat.dominic.francisc@gmail.com>
2023-04-02 12:46:48 +0300

    Double SIG UX testing

--- src/components/SettingsKeyPairsRegen.js
+++ src/components/SettingsKeyPairsRegen.js
@@ -11,13 +11,13 @@ function SettingsKeyPairsRegen(props) {
             <div>
                 <Label text="Key Pairs Regeneration" fontSize="2vh" style={{ left: '5%', top: '18.625%' }} color="#FFF" bkg="#7000FF00"></Label>
                 <Label text="If you suspect a device you authenticated has been compromised, regenerate your key pairs" fontSize="1.8vh" className="keysRegenLabel" style={{ width: '90%', top: '22.125%' }} color="#7000FF" bkg="#7000FF20"></Label>
-                <Label text="Regenerating your key pair will make all current conversations unreadable and your contacts will not be able to verify your identity until they would accept the new key signatures" fontSize="1.8vh" className="keysRegenLabel" style={{ width: '90%', top: '32.65625%', height: '12.5%' }} color="#FF002E" bkg="#FF002E20"></Label>
+                <Label text="Regenerating your key pairs will make all current conversations unreadable and your contacts will not be able to verify your identity until they would accept the new key signatures" fontSize="1.8vh" className="keysRegenLabel" style={{ width: '90%', top: '32.65625%', height: '12.5%' }} color="#FF002E" bkg="#FF002E20"></Label>
                 <div onClick={() => { }} id='exportIDButton' className='mainButton' style={{ border: 'solid 1px #FF001F', top: '47.0625%' }}>
                     <Label className="mainButtonLabel" text="Regenerate Key Pairs" color="#FF001F"></Label>
                     <KeysRegenDeco style={{ position: 'absolute', top: '-25%' }}></KeysRegenDeco>
                 </div>
-                <Signature top="80%"></Signature>
-                <Signature top="70%"></Signature>
+                <Signature sig2="bbsqc.svg42" doubleSig={true} height="15%" sigLabel="Own Encryption Key" verified={'self'} valid={true} sig="xxsqc.svg42" top="80%"></Signature>
+                <Signature sigLabel="Own SIG Verification Key" verified={'self'} valid={true} sig="xxxsq.sgf42" top="70%"></Signature>
                 <Button onClick={props.onBack} className="settingsMenuButton" style={{ top: '91.875%' }} fontSize="2.3vh" color="#929292" label="Back"></Button>
             </div>)
     } else {
--- src/components/Signature.js
+++ src/components/Signature.js
@@ -1,12 +1,16 @@
 
 import Barcode from 'react-barcode';
 import Label from '../components/Label.js'
+import BackDeco from '../components/BackDeco.js'
 
 function Signature(props) {
 
     const statusLabelsController = () => {
         if (props.valid) {
-            if (props.verified) {
+            if (props.verified == 'self') {
+                return 'Self'
+            }
+            if (props.verified == true) {
                 return 'Verified'
             } else {
                 return 'SIG Failed'
@@ -17,16 +21,36 @@ function Signature(props) {
     }
 
 
-    const colorHash = { 'Verified': '#00FF85', 'SIG Failed': '#FF002E', 'Invalid SIG': '#FF002E' }
+    const colorHash = { 'Verified': '#00FF85', 'SIG Failed': '#FF002E', 'Invalid SIG': '#FF002E', 'Self': '#9644FF' }
+
+    const sigLabelController = () => {
+        if(props.valid){
+            if(props.sig2){
+                return props.sig2;
+            }else{
+                return props.sig;
+            }
+        }else{
+            return '-UNKN';
+        }
+    }
 
     return (
         <div style={{ top: props.top, backgroundColor: `${colorHash[statusLabelsController()]}10` }} className='signature'>
             <Label color="#FFF" fontSize="1.9vh" text={props.sigLabel} className="sigIDLabel"></Label>
-            <Label color={colorHash[statusLabelsController()]} bkg={`${colorHash[statusLabelsController()]}30`} fontSize="1.8vh" text={statusLabelsController()} className="sigStatusLabel"></Label>
-            <Label color={colorHash[statusLabelsController()]} bkg={`${colorHash[statusLabelsController()]}30`} fontSize="1.8vh" text={props.valid ? props.sig : 'UNKNOWN'} className="sigActualLabel"></Label>
+            <Label color={colorHash[statusLabelsController()]} bkg={`${colorHash[statusLabelsController()]}30`} fontSize="1.8vh" text={props.doubleSig ? props.sig : statusLabelsController()} style={{left: '2%'}} className="sigStatusLabel"></Label>
+            <Label color={colorHash[statusLabelsController()]} bkg={`${colorHash[statusLabelsController()]}30`} fontSize="1.8vh" text={sigLabelController()} style={{left: `${props.doubleSig ? "30%" : "28%"}`}} className="sigActualLabel"></Label>
             <div className='sigcode'>
-                <Barcode format='CODE128' fontSize={0} width={1} height="30%" marginTop="18%" text="" lineColor={`${colorHash[statusLabelsController()]}DD`} background='#29005C00' value={props.valid ? props.sig : '-UNKNOWN-'}></Barcode>
+                <Barcode format='CODE128' fontSize={0} width={1} height={props.height ? props.height : "30%"} marginBottom={props.doubleSig ? "30%" : "0%"} marginTop={props.doubleSig ? "0%" : "18%"} text="" lineColor={`${colorHash[statusLabelsController()]}DD`} background='#29005C00' value={props.valid ? props.sig : '-UNKNOWN-'}></Barcode>
             </div>
+            {props.doubleSig ? 
+            <>
+            <div className='sigcode'>
+                <Barcode format='CODE128' fontSize={0} width={1} height={props.height ? props.height : "30%"} marginBottom={props.doubleSig ? "30%" : "0%"} marginTop={props.doubleSig ? "70%" : "18%"} text="" lineColor={`${colorHash[statusLabelsController()]}DD`} background='#29005C00' value={props.valid ? props.sig2 : '-UNKNOWN-'}></Barcode>
+            </div>
+            <BackDeco style={{ position: 'absolute', top: '41%', left: '59%', height: "25%", transform: 'rotate(-90deg)' }} color="#9644FF"></BackDeco>
+            <BackDeco style={{ position: 'absolute', top: '57%', left: '8%', height: "25%", transform: 'rotate(180deg)' }} color="#9644FF"></BackDeco>
+            </> : ''}
         </div>)
 }
 
