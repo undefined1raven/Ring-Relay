fe790f5f916ef166e50f2ca2855b71fec96cd78b
raven <zlat.dominic.francisc@gmail.com>
2023-04-26 22:50:22 +0300

    Active Request Type Deco Bug Fix

--- src/components/NewContactMatchDeco.js
+++ src/components/NewContactMatchDeco.js
@@ -4,10 +4,10 @@
 function NewContactMatchDeco(props) {
     return (
         <svg style={{position: 'absolute', left: '75.8%'}} width="12%" height="80%" viewBox="0 0 37 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect y="23" width="37" height="1" fill="#6300E0" />
-            <rect x="9" y="27" width="18" height="1" fill="#6300E0" />
-            <rect x="37" y="5" width="37" height="1" transform="rotate(-180 37 5)" fill="#6300E0" />
-            <rect x="28" y="1" width="18" height="1" transform="rotate(-180 28 1)" fill="#6300E0" />
+            <rect y="23" width="37" height="1" fill="#A055FF" />
+            <rect x="9" y="27" width="18" height="1" fill="#A055FF" />
+            <rect x="37" y="5" width="37" height="1" transform="rotate(-180 37 5)" fill="#A055FF" />
+            <rect x="28" y="1" width="18" height="1" transform="rotate(-180 28 1)" fill="#A055FF" />
         </svg>
     )
 
--- src/components/NewContactMatchTile.js
+++ src/components/NewContactMatchTile.js
@@ -6,21 +6,16 @@ import MsgRXDeco from '../components/MsgRXDeco.js'
 
 function NewContactMatchTile(props) {
     const RequestTypeController = () => {
-        if (props.type != undefined) {
-            if (props.type == 'Pending.TX') {
-                return <MsgTXDeco></MsgTXDeco>
-            } else {
-                return <MsgRXDeco></MsgRXDeco>
-            }
-        }
+
     }
     if (props.qid != localStorage.getItem('PKGetter')) {
         return (
             <div onClick={props.onClick} className="newContactMatchTileContainer">
                 <Label color="#FFF" className="newContactTileUsername" text={props.username}></Label>
-                <Label className="newContactQID" color="#6300E0" text={props.qid}></Label>
+                <Label className="newContactQID" color="#A055FF" text={props.qid}></Label>
                 <NewContactMatchDeco></NewContactMatchDeco>
-                {RequestTypeController()}
+                {props.type == 'Pending.TX' ? <MsgTXDeco bkg="#6100DC" opacity="0.3" color="#A055FF"></MsgTXDeco> : ''}
+                {props.type == 'Pending.RX' ? <MsgRXDeco bkg="#6100DC" opacity="0.3" color="#A055FF"></MsgRXDeco> : ''}
             </div>
         )
     }else{
